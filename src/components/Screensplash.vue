0b222355374df61ec2a20c4916d1b6fcda8cda86
Simone <s.mandola.design@hotmail.com>
2021-11-29 22:59:15 +0100

    screensplash

---
+++ src/components/Screensplash.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="screensplash">
+    <button type="button" title="Click to start">
+      <i class="icon__screensplash"></i>
+      <i class="icon__screensplash icon-shadow"></i>
+      <span class="dev-by">dev. by Simone Mandola</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Screensplash"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <h1>Get any GitHub user 😁</h1>
+    <Screensplash />
+    <h1 style="margin: 0">Get any GitHub user 😁</h1>
+    <button
+        type="button"><i class="icon__screensplash"
+                         style="width: 6rem; height: 6rem; font-size: 6rem; color: #fff;"></i></button>
   </div>
 </template>
 
 <script>
+
+import Screensplash from "@/components/Screensplash";
+
 export default {
-  name: "Home"
+  name: "Home",
+
+  components:{
+    Screensplash
+  }
 }
 </script>
